09a900fa648a948a7c7348ba33946417183db182
ib-m-j <ib.m.jorgensen@gmail.com>
2019-01-12 18:48:59 +0100

    started full deploy and moved mexicohigh to html project

--- deploy.py
+++ deploy.py
@@ -1,5 +1,8 @@
+import argparse
 import shutil
 import os.path
+import ftplib
+import io
 
 toDeploy = [
     "html/allmexico.html",
@@ -8,7 +11,8 @@ toDeploy = [
     "html/northwest.html",
     "html/east.html",
     "html/south.html",
-    "mexicohigh.html"
+    "../html/mexicohigh.html",
+    "../html/zoom.html"
     ]
 
 target = "../../Google\ Drive/hojoholding/html"
@@ -16,7 +20,33 @@ target = "../../Google\ Drive/hojoholding/html"
 
 
 
-for f in toDeploy:
-    print("Moving " + f)
-    shutil.copyfile(f, os.path.join("c:\\","Users","Ib", "Google Drive", "hojoholding", "html", os.path.basename(f)))
+def doDeploy():
+    for f in toDeploy:
+        print("Moving " + f)
+        shutil.copyfile(f, os.path.join("c:\\","Users","Ib", "Google Drive", "hojoholding", "html", os.path.basename(f)))
 
+def ftpDeploy():
+    ftpconn = ftplib.FTP(host="veramexicana.com", user="veramexi", passwd="PelleSoren1988")
+    #print(ftpconn.login())
+    print(ftpconn.cwd("public_html/getgoodtaste.com/Maps"))
+    print(ftpconn.retrlines('LIST'))
+    for f in toDeploy:
+        data = open(f, 'rb')
+        ftpconn.storlines('STOR {}'.format(os.path.basename(f)), data)
+        print('uploaded', os.path.basename(f))
+        data.close()
+
+    print(ftpconn.quit())
+    print("ended")
+
+        
+if __name__ == '__main__':
+    #below needed for ftp use string as file
+    openeds =io.StringIO("aslkfj\nasifu\n")
+    
+    while True:
+        l=openeds.readline()
+        if not l:
+            break
+        print(l)
+    openeds.close()
